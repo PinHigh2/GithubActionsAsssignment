d021d474518d6cde25f3fff336774a4000138896
PinHigh2 <carterwald67@gmail.com>
2025-10-27 21:12:12 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -61,7 +61,7 @@ public class Multiplication
     [TestMethod]
     public void Multiply_Valid_Patino()
     {
-        Assert.AreEqual(2000, Program.Multiply("1", "2"));
+        Assert.AreEqual(2, Program.Multiply("1", "2"));
         Assert.AreEqual(6, Program.Multiply("3", "2"));
         Assert.AreEqual(35, Program.Multiply("5", "7"));
     }
